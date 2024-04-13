44d435d765b2c22a4fa4d8bd73018830ae6e8b7a
IDontCareAboutUser <166947716+IDontCareAboutUser@users.noreply.github.com>
2024-04-13 14:34:22 -0400

    Add files via upload

---
+++ Offsets.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace OW {
+	namespace offset {
+		static constexpr auto Address_viewmatrix_base = 0x37655A8;
+		static constexpr auto Address_viewmatrix_base_test = 0x3E1E2B8;
+		static constexpr auto Address_entity_base = 0x3750B10;
+		static constexpr auto offset_viewmatrix_ptr = 0x7E0;
+		static constexpr auto offset_viewmatrix_xor_key = 0x28C363FCB19B8869;
+		static constexpr auto HeapManager = 0x38076EE;
+		static constexpr auto HeapManager_Var = 0x38234D0;
+		static constexpr auto HeapManager_Key = 0x24c4730cd926e72d;
+		static constexpr auto HeapManager_Pointer = 0x160;
+		static constexpr auto changefov = 0x38CBAC8;
+		static constexpr auto GetKeyAdd = 0x1EA393B;
+		static constexpr auto GetKeyAddRIP = 0x5;
+		static constexpr auto GlowESP = 0xE32400;
+		static constexpr auto Silent = 0x290069F;
+		static constexpr auto SensitivePtr = 0x2054;
+		static constexpr auto VisFN = 0x78B4E2;
+		static constexpr auto VisRead = 0x3826090;
+		static constexpr auto Vis_Key = 0x1558B33B5D2A15A;
+		static constexpr auto OutlineFN = 0x794432;
+		static constexpr auto OutlineRead = 0x3808640;
+		static constexpr auto OutLine_Key = 0x2062396461F92ACF;
+	}
+}
